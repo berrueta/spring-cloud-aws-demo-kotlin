410e5fa6f2981aa27ab617cf3acb50519b892bcb
Diego Berrueta <diego@berrueta.net>
2023-09-10 21:10:32 +1200

    Add example of class with the Jackson annotations

--- src/main/kotlin/com/example/demo/Message.kt
+++ src/main/kotlin/com/example/demo/Message.kt
@@ -1,3 +1,10 @@
 package com.example.demo
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+
+// Kotlin style (test does not pass)
 data class Message(val payload: String)
+
+// Java style (makes the test pass)
+//data class Message @JsonCreator constructor(@JsonProperty("payload") val payload: String)
