01d03be75a5375c9a642f34706a4873a7f1b5620
Diego Berrueta <diego@berrueta.net>
2023-09-11 08:04:36 +1200

    Move logging statement to the listener

--- src/main/kotlin/com/example/demo/MessageConsumer.kt
+++ src/main/kotlin/com/example/demo/MessageConsumer.kt
@@ -1,13 +1,10 @@
 package com.example.demo
 
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class MessageConsumer {
-    private val log = LoggerFactory.getLogger(this.javaClass)
-
     fun consumeMessage(message: Message) {
-        log.info("Received {}", message)
+        // no op
     }
 }
--- src/main/kotlin/com/example/demo/MessageListener.kt
+++ src/main/kotlin/com/example/demo/MessageListener.kt
@@ -1,14 +1,18 @@
 package com.example.demo
 
 import io.awspring.cloud.sqs.annotation.SqsListener
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class MessageListener(
         val messageConsumer: MessageConsumer
 ) {
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
     @SqsListener("demo-queue")
     fun receiveMessage(message: Message) {
+        log.info("Received {}", message)
         messageConsumer.consumeMessage(message)
     }
 }
